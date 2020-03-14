574a7ba560bc4cf25c0ef772c69ba719c691c319
TehCheat <TehCheat@users.noreply.github.com>
2020-03-14 13:49:14 -0400

    Updated IngameStateOffsetsstate

--- GameOffsets/IngameStateOffsets.cs
+++ GameOffsets/IngameStateOffsets.cs
@@ -7,25 +7,25 @@ namespace GameOffsets
     {
 		[FieldOffset(0x80)] public long IngameUi;
 		[FieldOffset(0xA0)] public long EntityLabelMap;
-		[FieldOffset(0x370)] public long Data;
-        [FieldOffset(0x378)] public long ServerData;
+		[FieldOffset(0x378)] public long Data;
+        [FieldOffset(0x380)] public long ServerData;
         [FieldOffset(0x4A8)] public long UIRoot;
-        [FieldOffset(0x4F0)] public long UIHover;
-        [FieldOffset(0x51C)] public float UIHoverX;
-        [FieldOffset(0x520)] public float UIHoverY;
-        [FieldOffset(0x4E0)] public long UIHoverTooltip;
-        [FieldOffset(0x4E8)] public float CurentUElementPosX;
-        [FieldOffset(0x4EC)] public float CurentUElementPosY;
-        [FieldOffset(0x570)] public int DiagnosticInfoType;
+		[FieldOffset(0x4E0)] public long UIHoverTooltip;
+		[FieldOffset(0x4E8)] public float CurentUElementPosX;
+		[FieldOffset(0x4EC)] public float CurentUElementPosY;
+		[FieldOffset(0x4F0)] public long UIHover;
+		[FieldOffset(0x518)] public int MouseXGlobal;
+		[FieldOffset(0x51C)] public int MouseYGlobal;
+		[FieldOffset(0x524)] public float UIHoverX;
+		[FieldOffset(0x528)] public float UIHoverY;
+		[FieldOffset(0x52C)] public float MouseXInGame;
+		[FieldOffset(0x530)] public float MouseYInGame;
+		[FieldOffset(0x554)] public float TimeInGame;
+		[FieldOffset(0x55C)] public float TimeInGameF;
+		[FieldOffset(0x570)] public int DiagnosticInfoType;
         [FieldOffset(0x7A0)] public long LatencyRectangle;
         [FieldOffset(0xC30)] public long FrameTimeRectangle;
         [FieldOffset(0xE78)] public long FPSRectangle;
-        [FieldOffset(0x554)] public float TimeInGame;
-        [FieldOffset(0x558)] public float TimeInGameF;
         [FieldOffset(0xFE4)] public int Camera;
-        [FieldOffset(0x518)] public int MouseXGlobal;
-        [FieldOffset(0x51C)] public int MouseYGlobal;
-        [FieldOffset(0x52C)] public float MouseXInGame;
-        [FieldOffset(0x530)] public float MouseYInGame;
     }
 }
